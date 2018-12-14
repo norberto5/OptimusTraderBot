88d6c14ac4752bd21d66380c33d4c0c21c56d92c
norberto5 <admin@norberto5.pl>
2018-12-15 00:02:39 +0100

    Changed TradeResult properties, added TradeInfo

--- OptimusTraderBot/Models/TradeResult.cs
+++ OptimusTraderBot/Models/TradeResult.cs
@@ -1,9 +1,36 @@
-﻿namespace OptimusTraderBot.Models
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace OptimusTraderBot.Models
 {
 	public class TradeResult
 	{
 		public bool Success { get; set; }
-		public int OrderId { get; set; }
-		//public string Error { get; set; }
+		[JsonProperty("order_id")]
+		public long OrderId { get; set; }
+		public decimal? Amount { get; set; }
+		public decimal? Rate { get; set; }
+		public decimal? Price { get; set; }
+		public decimal Fee { get; set; }
+		[JsonProperty("fee_currency")]
+		public string FeeCurrency { get; set; }
+		//public Dictionary<string, string> Wrong { get; set; }
+		public List<TradeInfo> Bought { get; set; }
+	}
+
+	public class TradeInfo
+	{
+		public decimal Count { get; set; }
+		public decimal Price { get; set; }
+		public decimal Rate { get; set; }
+		public decimal Active { get; set; } // ?
+		[JsonProperty("cc_id")]
+		public decimal CcId { get; set; } // ?
+		[JsonProperty("price_currency")]
+		public decimal PriceCurrency { get; set; }
+		[JsonProperty("price_is_crypto")]
+		public bool PriceIsCrypto { get; set; }
+		public DateTime Changed { get; set; }
 	}
 }
