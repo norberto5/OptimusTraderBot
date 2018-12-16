4bcde7b6310d1cda8c886f14e0a79924d6609950
norberto5 <admin@norberto5.pl>
2018-12-16 13:20:07 +0100

    Added Orderbook method api

---
+++ OptimusTraderBot/Models/OrderbookResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace OptimusTraderBot.Models
+{
+	public class OrderBookResult
+	{
+		public List<Order> Bids { get; set; }
+		public List<Order> Asks { get; set; }
+	}
+
+	public class Order
+	{
+		public string Currency { get; set; }
+		public decimal Price { get; set; }
+		public decimal Quantity { get; set; }
+
+		public decimal Rate => Price / Quantity;
+
+		public override string ToString() => $"{Price} {Currency} for {Quantity}";
+	}
+}
