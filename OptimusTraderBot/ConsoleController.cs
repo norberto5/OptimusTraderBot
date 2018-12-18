712d2e1f7e6e3e4b18fd7a8ae3498e394ba7e833
norberto5 <admin@norberto5.pl>
2018-12-18 19:45:52 +0100

    Added init of ConsoleController class

---
+++ OptimusTraderBot/ConsoleController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OptimusTraderBot.Models;
+
+namespace OptimusTraderBot
+{
+	public class ConsoleController
+	{
+		private readonly ApiManager apiManager;
+
+		public ConsoleController(ApiManager apiManager)
+		{
+			this.apiManager = apiManager;
+		}
+
+		public void ParseCommand(string cmdToParse)
+		{
+			string[] blocks = cmdToParse.Split(' ');
+			string cmd = blocks[0];
+
+			switch(cmd)
+			{
+				case "info":
+					InfoResult info = apiManager.GetInfo();
+					Console.WriteLine(new string('-', 30));
+					if(info.Success)
+					{
+						IEnumerable<KeyValuePair<string, Money>> balancesNotZero = info.Balances.Where(b => b.Value.Available != 0 || b.Value.Locked != 0);
+						if(balancesNotZero.Count() == 0)
+						{
+							Console.WriteLine("Your balances are empty!");
+						}
+						else
+						{
+							foreach(KeyValuePair<string, Money> balance in balancesNotZero)
+							{
+								Console.WriteLine($"{balance.Key} : {balance.Value}");
+							}
+						}
+						Console.WriteLine(new string('-', 30));
+						Console.WriteLine($"Fee: {info.Fee}");
+					}
+					else
+					{
+						Console.WriteLine("Command failed!");
+					}
+					break;
+			}
+		}
+	}
+}
