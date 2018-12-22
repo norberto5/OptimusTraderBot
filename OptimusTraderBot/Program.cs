4c215ba122f827841804e78c7ac9bbf321653f0f
norberto5 <admin@norberto5.pl>
2018-12-23 00:20:18 +0100

    Improved Program, added automatic refresh

--- OptimusTraderBot/Program.cs
+++ OptimusTraderBot/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
+using System.Text;
+using System.Threading;
 using Microsoft.Extensions.Configuration;
 using OptimusTraderBot.Models;
 using OptimusTraderBot.Settings;
@@ -10,6 +13,8 @@ namespace OptimusTraderBot
 	internal class Program
 	{
 		public static readonly CultureInfo DateCulture = CultureInfo.GetCultureInfo("PL");
+
+		private static ApiManager apiManager;
 
 		private static void Main(string[] args)
 		{
@@ -23,17 +28,48 @@ namespace OptimusTraderBot
 			var userSettings = new UserSettings();
 			config.GetSection(nameof(UserSettings)).Bind(userSettings);
 
-			var apiManager = new ApiManager(apiSettings);
+			apiManager = new ApiManager(apiSettings);
 
+			var backgroundWorker = new BackgroundWorker();
+			backgroundWorker.DoWork += new DoWorkEventHandler(BackgroundWorkerDoWork);
+			backgroundWorker.WorkerSupportsCancellation = true;
+
+			WelcomeMessage();
+
+			var consoleController = new ConsoleController(apiManager, userSettings);
+			string cmd = string.Empty;
+			while(cmd != "exit")
+			{
+				if(Console.KeyAvailable)
+				{
+					if(backgroundWorker.IsBusy)
+					{
+						backgroundWorker.CancelAsync();
+						Console.WriteLine("Auto update cancelled");
+					}
+
+					cmd = Console.ReadLine();
+
+					if(cmd == "auto" || string.IsNullOrEmpty(cmd))
+					{
+						if(!backgroundWorker.IsBusy)
+							backgroundWorker.RunWorkerAsync();
+					}
+					else
+					{
+						consoleController.ParseCommand(cmd);
+					}
+				}
+
+			}
+
+			Console.Read();
+		}
+
+		private static void WelcomeMessage()
+		{
 			InfoResult info = apiManager.GetInfo();
 			Console.WriteLine($"Connection to API is {(info.Success ? "OK" : "NOT OK! Something went wrong!")}");
-
-			//TradeResult tradeResult = apiManager.Trade(TradeType.Buy, "LSK", 0.123m, "PLN", 1m);
-			//long orderId = tradeResult.OrderId;
-			//if(orderId != 0)
-			//{
-			//	CancelResult cancelResult = apiManager.CancelOrder(orderId);
-			//}
 
 			Console.WriteLine("Actual orders:");
 			List<Order> orders = apiManager.GetOrders();
@@ -43,21 +79,60 @@ namespace OptimusTraderBot
 			Console.WriteLine(new string('-', 30));
 
 			Console.WriteLine("Actual OrderBook for LSK/PLN:");
-			OrderBookResult orderbookResult =apiManager.GetOrderBook("LSK", "PLN");
+			OrderBookResult orderbookResult = apiManager.GetOrderBook("LSK", "PLN");
 			Console.WriteLine(orderbookResult.GetString(3, 3));
 
 			Console.WriteLine(new string('-', 30));
-			//apiManager.GetTransactions();
+		}
 
-			var consoleController = new ConsoleController(apiManager, userSettings);
-			string cmd = string.Empty;
-			while(cmd != "exit")
+		private static void BackgroundWorkerDoWork(object sender, DoWorkEventArgs e)
+		{
+			List<Order> orders = apiManager.GetOrders();
+			while(true)
 			{
-				cmd = Console.ReadLine();
-				consoleController.ParseCommand(cmd);
-			}
+				var worker = sender as BackgroundWorker;
+				if(worker.CancellationPending)
+				{
+					e.Cancel = true;
+					return;
+				}
 
-			Console.Read();
+				var sb = new StringBuilder();
+				sb.AppendLine($"------- AUTO UPDATE ------- ({DateTime.Now.ToString(DateCulture)})");
+				sb.AppendLine("Actual orders:");
+				List<Order> newOrders = apiManager.GetOrders();
+				foreach(Order order in newOrders)
+					sb.AppendLine(order.ToString());
+
+				sb.AppendLine(new string('-', 30));
+
+				sb.AppendLine("Actual OrderBook for LSK/PLN:");
+				OrderBookResult orderbookResult = apiManager.GetOrderBook("LSK", "PLN");
+				sb.AppendLine(orderbookResult.GetString(5, 5));
+
+				Console.Clear();
+				Console.WriteLine(sb);
+
+				if(orders.Count != newOrders.Count)
+				{
+					orders = newOrders;
+					Console.Beep(500, 500);
+					Console.Beep(500, 500);
+					Console.Beep(500, 500);
+					Console.WriteLine("Orders changed!");
+					break;
+				}
+
+				for(int i = 0; i < 100; i++)
+				{
+					if(worker.CancellationPending)
+					{
+						e.Cancel = true;
+						return;
+					}
+					Thread.Sleep(100);
+				}
+			}
 		}
 	}
 }
